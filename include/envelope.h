9d1a5c299e67e550a7713a618e7a6847798c14ca
Sebastian Melinat <melinat@keenlogics.com>
2015-10-14 16:45:42 +0200

    Envelope copy constructor calls base class with passed envelop

--- include/envelope.h
+++ include/envelope.h
@@ -67,7 +67,8 @@ public:
      *
      *  @param  envelope    the envelope to copy
      */
-    Envelope(const Envelope &envelope) : MetaData(),
+    Envelope(const Envelope &envelope) :
+        MetaData(envelope),
         _str(envelope._body, envelope._bodySize),
         _body(_str.data()),
         _bodySize(_str.size())
